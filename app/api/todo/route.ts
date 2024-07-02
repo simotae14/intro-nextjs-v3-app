2eeb9d75d3f66530378e8d6c575e6649c4bc1487
Simone Taeggi <startae14@gmail.com>
2024-07-02 23:02:25 +0200

    Add lint ignore to pass deploy

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,16 +1,13 @@
-import { NextResponse } from 'next/server'
 import db from '@/utils/db'
+import { NextResponse } from 'next/server'
 
-export const GET = async (request: Request) => {
-  const todos = await db.todo.findMany({})
-  return NextResponse.json({ data: todos })
+export const GET = async () => {
+  const data = await db.todo.findMany({})
+  return NextResponse.json({ data })
 }
 
 export const POST = async (request: Request) => {
   const data = await request.json()
-  // update the database
-  const todo = await db.todo.create({
-    data,
-  })
-  return NextResponse.json({ message: todo })
+  const todo = await db.todo.create({ data })
+  return NextResponse.json({ data: todo })
 }
